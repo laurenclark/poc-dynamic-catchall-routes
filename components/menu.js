6d23bf5adf7e432330ae9314ee4141aaf64c631a
Lauren Clark <lauren.c4231@gmail.com>
2022-06-24 23:55:33 +0100

    Refactor

--- components/menu.js
+++ components/menu.js
@@ -1,52 +1,34 @@
-import { memo } from "react";
 import NextLink from "next/link";
 
 import { ListItem, List, LinkBox } from "@chakra-ui/react";
 
 const createMenu = (items) => {
+    const withoutChildren = ({ name, url }) => (
+        <LinkBox color="orange">
+            <NextLink href={url || "/"} passHref>
+                {name}
+            </NextLink>
+        </LinkBox>
+    );
+
+    const withChildren = ({ name, children }) => (
+        <>
+            {name} ▼{createMenu(children)}
+        </>
+    );
     return (
         <List>
             {items?.map(({ name, url, children }, i) => (
                 <ListItem key={i}>
-                    {!children ? (
-                        <LinkBox color="orange">
-                            <NextLink href={url || "/"} passHref>
-                                {name}
-                            </NextLink>
-                        </LinkBox>
-                    ) : (
-                        <>
-                            {name} func ▼{createMenu(children)}
-                        </>
-                    )}
+                    {!children
+                        ? withoutChildren({ name, url })
+                        : withChildren({ name, children })}
                 </ListItem>
             ))}
         </List>
     );
 };
 
-function Menu({ menuItems }) {
-    return (
-        <>
-            <List padding="0">
-                {menuItems?.map(({ name, url, children }, i) => (
-                    <ListItem key={i}>
-                        {!children ? (
-                            <LinkBox color="orange">
-                                <NextLink href={url || "/"} passHref>
-                                    {name}
-                                </NextLink>
-                            </LinkBox>
-                        ) : (
-                            <>
-                                {name} orig ▼{createMenu(children)}
-                            </>
-                        )}
-                    </ListItem>
-                ))}
-            </List>
-        </>
-    );
+export default function Menu({ menuItems }) {
+    return createMenu(menuItems);
 }
-
-export default Menu;
